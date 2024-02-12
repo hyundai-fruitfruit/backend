050a5ae9934daae2cad375b34d4ed3a3178643cc
sooyoung <sooyoung.h8@gmail.com>
2024-02-13 08:00:51 +0900

    feat: 회원 도메인 추가

---
+++ src/main/java/com/hyundai/app/member/domain/Member.java
@@ -0,0 +1,25 @@
+package com.hyundai.app.member.domain;
+
+import com.hyundai.app.common.entity.BaseEntity;
+import com.hyundai.app.member.enumType.Role;
+import lombok.*;
+
+/**
+ * @author 황수영
+ * @since 2024/02/12
+ * 회원 도메인
+ */
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    private int id;
+    private String email;
+    private String nickname;
+    private Role role;
+    private String refreshToken;
+    private String oauthId;
+}
---
+++ src/main/java/com/hyundai/app/member/enumType/Role.java
@@ -0,0 +1,13 @@
+package com.hyundai.app.member.enumType;
+
+import lombok.Getter;
+
+/**
+ * @author 황수영
+ * @since 2024/02/12
+ * 회원 타입
+ */
+@Getter
+public enum Role {
+    ROLE_MEMBER, ROLE_ADMIN, ROLE_GUEST;
+}
