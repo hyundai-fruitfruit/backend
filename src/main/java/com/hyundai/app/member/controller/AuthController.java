fa2cf093c5acde63cce73b8ed95ba186b90b7354
sooyoung <sooyoung.h8@gmail.com>
2024-02-14 06:48:20 +0900

    fix: 서비스단 구현체 주입시, @Qualifie 사용하도록 변경

--- src/main/java/com/hyundai/app/member/controller/AuthController.java
+++ src/main/java/com/hyundai/app/member/controller/AuthController.java
@@ -2,10 +2,13 @@ package com.hyundai.app.member.controller;
 
 import com.hyundai.app.member.dto.LoginReqDto;
 import com.hyundai.app.member.dto.LoginResDto;
+import com.hyundai.app.member.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +21,18 @@ import org.springframework.web.bind.annotation.*;
 @Log4j
 @Api("인증/인가용 API")
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    // @Qualifier("MemberServiceImpl")
-    private final com.hyundai.app.member.service.MemberServiceImpl memberServiceImpl;
+    @Autowired
+    @Qualifier("memberServiceImpl")
+    private MemberService memberService;
 
     @PostMapping("/login")
     @ApiOperation("회원가입/로그인 API")
     public ResponseEntity<LoginResDto> login(@RequestBody LoginReqDto loginReqDto) {
         log.debug("로그인 시도 => 토큰 : " + loginReqDto.getLoginToken());
-        LoginResDto loginResDto = memberServiceImpl.login(loginReqDto);
+        LoginResDto loginResDto = memberService.login(loginReqDto);
         return new ResponseEntity<>(loginResDto, HttpStatus.ACCEPTED);
-
     }
 }
